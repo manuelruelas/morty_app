32b55729a0098b0e9e18add9cbdba75174aa7a19
Manuel Ruelas <manuel.r.a@hotmail.com>
2026-07-08 14:57:57 -0700

    Refactor dependency injection setup in injection.config.dart

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -9,8 +9,12 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+
+import '../network/api_client.dart' as _i557;
+import 'register_module.dart' as _i291;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -18,7 +22,12 @@ extension GetItInjectableX on _i174.GetIt {
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final registerModule = _$RegisterModule();
+    gh.lazySingleton<_i361.Dio>(() => registerModule.dio);
+    gh.lazySingleton<_i557.ApiClient>(() => registerModule.apiClient);
     return this;
   }
 }
+
+class _$RegisterModule extends _i291.RegisterModule {}
