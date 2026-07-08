2253cad66b101bf289febfc68c829cfdda9fa435
Manuel Ruelas <manuel.r.a@hotmail.com>
2026-07-08 15:08:34 -0700

    Initialize Flutter bindings and configure dependencies in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:morty_app/core/di/injection.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
   runApp(const MainApp());
 }
 
