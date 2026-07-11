7dc3e7b4b69ced0f354822f5c5c32ae0a956e33b
Manuel Ruelas <manuel.r.a@hotmail.com>
2026-07-10 20:51:06 -0700

    Add unit tests for LocationBloc to verify location fetching functionality

--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -6,6 +6,7 @@ import 'package:morty_app/features/character/domain/usecases/get_characters.dart
 import 'package:morty_app/features/character/domain/usecases/get_favorite_characters.dart';
 import 'package:morty_app/features/character/domain/usecases/toggle_favorite_character.dart';
 import 'package:morty_app/features/episode/domain/usecases/get_episodes.dart';
+import 'package:morty_app/features/location/domain/usecases/get_locations.dart';
 
 class MockCharacterRemoteDataSource extends Mock
     implements CharacterRemoteDataSource {}
@@ -26,3 +27,5 @@ class MockGetEpisodes extends Mock implements GetEpisodes {}
 
 class MockToggleFavoriteCharacter extends Mock
     implements ToggleFavoriteCharacter {}
+
+class MockGetLocations extends Mock implements GetLocations {}
